db7d960f422f766fcfbdd148ee8f0e31ad69b918
LuisVasquez13 <luis.vasquez14@utp.ac.pa>
2025-10-26 19:04:52 -0500

    Añadido comentarios

--- Program.cs
+++ Program.cs
@@ -6,9 +6,10 @@ namespace SoftIV_CalculadoraBasica // namespace
     // Clase principal
     internal static class Program
     {
-        [STAThread]
-        static void Main()
+        [STAThread] 
+        static void Main() // Main
         {
+            // Se inicializa en calculadora
             ApplicationConfiguration.Initialize();
             Application.Run(new FormCalculadora()); // Inicia la calculadora
         }
